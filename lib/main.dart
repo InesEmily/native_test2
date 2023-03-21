c07a09673d76472dd3757fdea7bacd7f7431f29e
inese <inesemilymertens@gmail.com>
2023-03-21 09:45:56 +0100

    oefening 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/models/data.dart' as lib;
 
 void main() {
   runApp(MainApp());
@@ -11,14 +12,52 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class Main extends StatelessWidget {
+class Main extends StatefulWidget {
   const Main({super.key});
 
   @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  String dropdownvalue = 'All';
+  var items = ['All', 'Resistance', 'Sith', 'Jedi Order'];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('In a galaxy far far away...'),
+      appBar: AppBar(title: Text('Star Wars Characters'), centerTitle: true),
+      body: Column(
+        children: [
+          Align(alignment: Alignment.topLeft, child: Text('Select a side:')),
+          DropdownButton(
+            // Initial Value
+            value: dropdownvalue,
+
+            // Down Arrow Icon
+            icon: const Icon(Icons.keyboard_arrow_down),
+
+            // Array list of items
+            items: items.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(items),
+              );
+            }).toList(),
+            // After selecting the desired option,it will
+            // change button value to selected value
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownvalue = newValue!;
+              });
+            },
+          ),
+          Center(
+            child: Text(
+              'There are ${lib.characters.length} charachters',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
       ),
     );
   }
